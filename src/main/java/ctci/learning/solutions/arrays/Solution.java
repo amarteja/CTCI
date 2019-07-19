6edb6d9984dcc15e757abe01a7b10a156e85935b
Amar C <amarteja@gmail.com>
2019-07-18 23:02:44 -0500

    Updated packages

---
+++ src/main/java/ctci/learning/solutions/arrays/Solution.java
@@ -0,0 +1,38 @@
+package ctci.learning.solutions.arrays;
+
+
+public class Solution {
+
+    /**
+     * Implement an algorithm to determine if a string has all unique characters, What if you cannot use additional data structure.
+     */
+    public static boolean isUnique(String str)
+    {
+        return false;
+    }
+
+    /**
+     * Given two strings,write a method to decide if one is a permutaion of other.
+     */
+    public static boolean checkPermutation(String str1, String str2)
+    {
+        return false;
+    }
+
+
+    /**
+     * Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold
+     * the additional characters, and that you are given the 'true'length of the string.(Note :use character array to
+     * perform the operation in place).
+     */
+    public static String urlify(String str)
+    {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Solution.isUnique("Hello World"));
+    }
+
+}
